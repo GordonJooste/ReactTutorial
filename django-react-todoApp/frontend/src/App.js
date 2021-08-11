32f03cc50ed9dbe70cf8fae7a5d257ea2989240a
ThABlEh <jooste44@gmail.com>
2021-08-11 11:52:27 +0700

    started on Django-React App Tutorial, made basic backend API and React APp, need to connect them so they can communicate now

---
+++ django-react-todoApp/frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+
+const todoItems= [
+  {
+    id: 1,
+    title: "Nature walk in the park",
+    description: "Visit the park with my friends",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Visit",
+    description: "Got to my aunt's place",
+    completed: true
+  },
+  {
+    id: 3,
+    title: "Write",
+    description: "Do an article about anthropology",
+    completed: true
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {todoItems};
+  };
+
+  render() {
+    return (
+      <main className="content">
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush">
+                {this.state.todoItems.map(Item => (
+                  <div>
+                    <h1>{Item.title}</h1>
+                    <span>{Item.description}</span>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+export default App;
